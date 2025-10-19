0ca0981deb49c23064134592d25dffb0f83b1734
liptonn405 <danillihanov321@gmail.com>
2025-10-19 10:20:24 +0300

    Add new file

---
+++ src/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5];
+    int n = 0;
+    int num;
+
+    while (scanf("%d", &num) == 1 && n < 5) {
+        arr[n] = num;
+        n++;
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%d", arr[i]);
+        if (i < n - 1) {
+            printf(" ");
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
